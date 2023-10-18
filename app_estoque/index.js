9e2dd8b44ebc950f2d6a963317db859c9b1fc503
JoaoLucasKnight <kirajota1@gmail.com>
2023-10-18 07:37:14 -0300

    up

--- app_estoque/index.js
+++ app_estoque/index.js
@@ -11,16 +11,16 @@ app.get("/api/adicionar/:id/:nome/:qtd", function(req, res){
     let nome = req.params.nome;
     let qtd = req.params.qtd;
 
-    let p = estoque.criar_produto(id, nome, qtd);
-    estoque.adicionar_produto(p);
-    res.json(p);
+    let p = estoque.criar_produto(id, nome, qtd); // cria o objeto 
+    estoque.adicionar_produto(p); // passa o objeto para o array 
+    res.json(p); // retorna somente o obejto criado 
 });
 
-app.get("/api/listar", function (req, res) {
-    res.json(estoque.listar_produtos()); 
+app.get("/api/listar", function (req, res) {  
+    res.json(estoque.listar_produtos()); // retonar todo o array de objeto
 });
 
-app.get("/api/editar/:id/:qtdAtual", function (req, res) {
+app.get("/api/editar/:id/:qtdAtual", function (req, res) {  // funçãoq ue so altaera a quantidade, realizando a busca pelo ID
     let id = req.params.id;
     let qtdAtual = req.params.qtdAtual;
     res.json(estoque.editar_produto(id, qtdAtual));
