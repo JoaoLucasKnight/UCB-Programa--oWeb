956012162f973c76e5b370d37be2b588253d5538
JoaoLucasKnight <kirajota1@gmail.com>
2023-10-25 08:39:02 -0300

    Atividade6

--- pw-atividade/index.js
+++ pw-atividade/index.js
@@ -6,9 +6,22 @@ app.engine('html', bigode());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views')
 
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req,res) =>{
     res.render('index.html')
-})
+});
+
+app.post('/dados', (req,res) =>{
+    let dados ={
+        nome: req.body.nome,
+        endereço: req.body.address,
+        telefone: req.body.telefone,
+        data: req.body.data  
+    };
+    res.render('dados.html',{dados})
+});
+
 
 const PORT = 8080;
 app.listen(PORT, function (){
