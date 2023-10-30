e93512e5b01f763065e3cd2bc2bd4d2eee6fa48e
JoaoLucasKnight <kirajota1@gmail.com>
2023-10-30 08:46:33 -0300

    fazendo a atividade 7

--- Atividade7/index.js
+++ Atividade7/index.js
@@ -2,8 +2,33 @@ const express = require ('express');
 const bigode  = require ('mustache-express');
 const app = express();
 
+//render msutache 
+app.engine('html', bigode());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/pages')
+
+app.use(express.urlencoded({extended: true}))
+
 app.get('/' , (req,res)=> {
-    res.send("Ola mundo")
+    res.render('form.html')
+})
+
+app.post('/dados',(req,res)=>{
+    let dados ={
+        nome:req.body.nome,
+        CPF:req.body.cpf,
+        Data_de_nascimento:req.body.data,
+        Endereço:req.body.address,
+        UF:req.body.uf,
+        CEP:req.body.cep,
+        Email:req.body.email,
+        Telefone:req.body.telefone,
+        Especialidade:req.body.especialidade,
+        Alergico:req.body.aler,
+        Informações:req.body.info,
+    }
+
+    res.render('dados.html', {dados})
 })
 
 const PORTA = 8080;
