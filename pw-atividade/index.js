96dc810a169c8bcf2a8cb93ceb4dc6ed674d579d
JoaoLucasKnight <kirajota1@gmail.com>
2023-10-30 06:45:58 -0300

    Atividade 6

--- pw-atividade/index.js
+++ pw-atividade/index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const bigode = require('mustache-express')
-const app     = express();
+const express = require('express');        // express pusao para rodar no serve 
+const bigode = require('mustache-express') // mustache rebderizador 
+const app     = express();                 // aqui passamos todas as funções para dentro da const app 
 
+
+// Aqui definimos onde e como o mustache vai renderizar o html 
 app.engine('html', bigode());
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/views')  // indica o caminhos das paginas (__dirname fornece o caminho do arquivo atual )
 
+// nas requisições tipo post os dados são passado no corpo, e com a declaração abaixo conseguimos acessar as info do boy a requisição 
 app.use(express.urlencoded({extended: true}))
 
+// rota base
 app.get('/', (req,res) =>{
     res.render('index.html')
 });
 
+
+// rota post 
 app.post('/dados', (req,res) =>{
     let dados ={
         nome: req.body.nome,
