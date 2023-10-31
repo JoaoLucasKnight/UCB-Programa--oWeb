e83b711d3d891041aeb2e932cd51d841a89c2d69
JoaoLucasKnight <kirajota1@gmail.com>
2023-10-31 07:06:39 -0300

    finalizado atividade 7

--- Atividade7/index.js
+++ Atividade7/index.js
@@ -7,6 +7,7 @@ app.engine('html', bigode());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/pages')
 
+// lida com dados de formulário no formato URL-encoded. CHAVE-VALOR { extended: true } permite que você lide com objetos mais complexos, como arrays e objetos aninhados, nos dados do formulário.
 app.use(express.urlencoded({extended: true}))
 
 app.get('/' , (req,res)=> {
@@ -18,17 +19,17 @@ app.post('/dados',(req,res)=>{
         nome:req.body.nome,
         CPF:req.body.cpf,
         Data_de_nascimento:req.body.data,
-        Endereço:req.body.address,
+        Endereço:req.body.endereço,
         UF:req.body.uf,
         CEP:req.body.cep,
         Email:req.body.email,
         Telefone:req.body.telefone,
         Especialidade:req.body.especialidade,
-        Alergico:req.body.aler,
-        Informações:req.body.info,
+        Alergico:req.body.alergias,
+        Informações:req.body.informações,
     }
 
-    res.render('dados.html', {dados})
+    res.render('dados.html', {dados}) // passo a variavel dados com os dados do furmuliro pra pagina /dados 
 })
 
 const PORTA = 8080;
